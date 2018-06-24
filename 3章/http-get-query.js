821c36038a2f91de6e528fdc7d47619ce0352dcf
tomonari_t <tomo1208.japan@gmail.com>
2018-06-24 18:56:09 +0900

    p74: パラメーター付きGET実装

---
+++ 3章/http-get-query.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const querystring = require('querystring')
+
+const query = {
+  hoo: 'test/test'
+}
+http.get(`http://localhost:18888?${querystring.stringify(query)}`, (res) => {
+  const { statusCode } = res
+  console.log(statusCode)
+  if (statusCode !== 200) {
+    console.error(`Status code: ${statusCode}`)
+    res.resume()
+    return
+  }
+
+  res.setEncoding('utf8')
+  const data = []
+  res.on('data', chunk => { data.push(chunk) })
+  res.on('end', () => {
+    const body = data.join('')
+    console.log(body)
+  })
+
+}).on('error', e => {
+  console.error(e.message)
+})
